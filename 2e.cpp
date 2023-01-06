0a5bdb239291f3465e6a3f4298c7a4ef7299da5c
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-01-06 13:36:53 +0300

    Create 2e.cpp

---
+++ 2e.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+float x, min, max;
+
+void data_input() {
+    std::cout << "Enter step - ";
+    std::cin >> x;
+    std::cout << "\nEnter min - ";
+    std::cin >> min;
+    std::cout << "\nEnter max - ";
+    std::cin >> max;
+}
+int main()
+{
+    data_input();
+    if (max < min) {
+        std::cout << "\nmin bigger than max";
+    }
+    while (min < max) {
+        std::cout << "\nX-" << min << " Y-" << pow(2 + min, 2) + 3 * min;
+        min = min + x;
+    }
+}
